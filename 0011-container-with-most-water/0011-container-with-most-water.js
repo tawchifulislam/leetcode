a88badc9d844f291ada86069406ae71e64d952d7
Tawchiful Islam <tawchif04@gmail.com>
2026-08-03 08:45:24 +0600

    Time: 3 ms (48.8%), Space: 61.6 MB (93.45%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxWater = 0;
+
+    while (left < right) {
+        const width = right - left;
+        const currentHeight = Math.min(height[left], height[right]);
+        const currentArea = width * currentHeight;
+
+        maxWater = Math.max(maxWater, currentArea);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxWater;
+};
