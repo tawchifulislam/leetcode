d8ae9a547434f1c0f492b95db39c525746547c0b
Tawchiful Islam <tawchif04@gmail.com>
2026-08-16 21:28:36 +0600

    Time: 2 ms (100%), Space: 67.6 MB (72.73%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} stones
+ * @return {boolean}
+ */
+var stoneGameIX = function(stones) {
+    const cnt = [0, 0, 0];
+    for (const stone of stones) {
+        cnt[stone % 3]++;
+    }
+
+    if (cnt[0] % 2 === 0) {
+        return cnt[1] >= 1 && cnt[2] >= 1;
+    }
+    return Math.abs(cnt[1] - cnt[2]) > 2;
+};
